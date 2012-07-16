afc3063f4cbc9f72167d7d6c5a0508bde4d758a9
Nick Williams <WickyNilliams@MBA>
2012-07-16 01:27:35 +0100

    more bookmarklet checking in

---
+++ src/LinkChecker.Bookmarklet.js
@@ -0,0 +1,39 @@
+(function() {
+    //modify these as you see fit
+    var jQueryUrl = "https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.js",
+        linkCheckerUrl = "https://github.com/WickyNilliams/LinkChecker/blob/master/src/LinkChecker.js",
+        linkCheckerUiUrl = "https://github.com/WickyNilliams/LinkChecker/blob/master/src/LinkChecker.UI.js";
+
+    function loadScript(url, callback) {
+
+        var head = document.getElementsByTagName("head")[0];
+        var script = document.createElement("script");
+        script.src = url;
+
+        // Attach handlers for all browsers
+        script.onload = script.onreadystatechange = function() {
+            var loaded = this.readyState === "loaded",
+                completed = this.readyState === "complete";
+
+            if(!this.readyState || loaded || completed) {
+
+                if(callback) callback();
+
+                // Handle memory leak in IE
+                script.onload = script.onreadystatechange = null;
+                head.removeChild( script );
+            }
+        };
+
+        head.appendChild(script);
+    }
+
+    //Daisy-chain script loading, bad form but functional
+    loadScript(jQueryUrl, function() {
+        loadScript(linkCheckerUrl, function() {
+            loadScript(linkCheckerUiUrl);
+        })
+    })
+
+}());
+
